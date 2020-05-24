d9425374c16273f407c5283e90163385388cdc09
Glitch (yakcollective-ifttt-connector) <none>
2020-05-24 17:39:32 +0000

    🐸🤑 Updated with Glitch

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# ifttt-ghpages
+# yakcollective-ifttt-connector
 
-Hello! 👋 This is a script which lets you create Jekyll entries in a Github repo from IFTTT events, using the IFTTT Webhook action. You can see it in use on [https://webrender.net](https://webrender.net).
+A script that creates Jekyll posts in a Github repo from IFTTT events, using the IFTTT Webhook action. Lightly modified from [the example connector](https://glitch.com/~ifttt-ghpages) [described on `webrender.net`](https://webrender.net/2017/11/23/automate-github-pages-ifttt-glitch.html)
 
-# Setup
+## Setup
 
-* [Remix this project](https://glitch.com/~ifttt-ghpages) (or [clone the repo](https://github.com/webrender/ifttt-ghpages) if you'd prefer to run it yourself)
-* in the .env file, enter your username, the repo you'd like to commit to, and your Github user token.  You can generate a Personal Access Token for use with this script here: [https://github.com/settings/tokens](https://github.com/settings/tokens)
-* for the `WEBHOOK_TOKEN` env variable, generate/create a token of your choice.  We'll use this to verify requests are coming from our webhook.
-* Go to IFTTT and create a new applet. For the "that" action, choose the `Webhooks` service, and then the `Make a web request` option.  Fill in the fields as follows:
-  * URL: The URL of your node server, with a `token` parameter. Example:
-    * `https://ifttt-ghpages.glitch.me/?token=your-token`
-  * Method: POST
-  * Content Type: text/plain
-  * Body: A YAML-formatted Jekyll document, with newlines replaced with `|||`. Example from `examples/twitter`:
-    * `---|||layout: post|||title: {{UserName}} - Tweet|||network: twitter|||date: {{CreatedAt}}|||---|||{{TweetEmbedCode}}`
+- Copy `_env` to `.env`.
+- Set `GH_USER`, `GH_REPO`, and `GH_BRANCH` appropriately.
+- Set `GH_TOKEN` to a [GitHub **Personal Access Token**](https://github.com/settings/tokens).
+- Set `WEBHOOK_TOKEN` to a shared secret of your choice (the output of `uuidgen` works well).  We'll use this to verify requests are coming from our webhook.
+- Go to IFTTT and create a new applet. For the "that" action, choose the **Webhooks** service, and then the **Make a web request** option.  Fill in the fields as follows:
+  - The **URL** of your node server, with a `token` parameter. Optionally include a `category` parameter (posts will be stored in `$category/_posts`). For example: `https://ifttt-ghpages.glitch.me/?token=your-token&category=writings`
+  - Use `POST` for the **Method**.
+  - Use `text/plain` for the **Content Type**.
+  - For the **Body**, create an HTML file with YAML frontmatter. Replace the newlines in the YAML with `|||`. For example:`---|||layout: post|||title: {{UserName}} - Tweet|||network: twitter|||date: {{CreatedAt}}|||---|||{{TweetEmbedCode}}`
--- server.js
+++ server.js
@@ -1,62 +1,120 @@
-// server.js
-// where your node app starts
+// Load required packages
+//
+var express = require("express");
+var bodyParser = require("body-parser");
+var moment = require("moment");
+var chrono = require("chrono-node");
+var accents = require('remove-accents');
+var { Octokit } = require("@octokit/rest");
 
-// init project
-var express = require('express');
-var bodyParser = require('body-parser');
-var moment = require('moment');
-var chrono = require('chrono-node');
-var app = express();
-var { Octokit } = require('@octokit/rest');
-var moment = require('moment');
-
+// Init GitHub connection
+//
 var github = new Octokit({
   auth: process.env.GH_TOKEN
 });
 
+// Init server
+//
+var app = express();
 app.use(bodyParser.text());
+
+// Do things with incoming requests
+//
 app.post("/", (req, res) => {
-  if (req.query.token !== process.env.WEBHOOK_TOKEN)
+  console.info("Received request at " + moment().format("YYYY-MM-DD HH:mm:ss ZZ"));
+  
+  // Reject requests without the right secret
+  //
+  if (req.query.token !== process.env.WEBHOOK_TOKEN) {
+    console.error("Got bad secret \"" + req.query.secret + "\" (should have been \"" + process.env.WEBHOOK_SECRET + "\")");
     res.sendStatus(400);
-  var body = req.body.replace(/\|\|\|/g, '\n');
-  body = body.replace('INSERT_DATE_HERE', moment(new Date()).subtract(10, 'hours').format("MMMM D, YYYY [at] HH:mmA"));
-  console.log(body)
-  if (body.includes('post via ifttt-ghpages')) { 
-    // don't post commits from this script, causing an infinite post loop
-    res.sendStatus(200)
-  } else {
-    var title;
-    var titleSearch = body.match(/title: (.*?)\n/);
-    if (titleSearch && titleSearch.length > 0) {
-      title = titleSearch[1];
-    }
-    var dateSearch = body.match(/date: (.*?)\n/);
-    var date = dateSearch[1] ? chrono.parseDate(dateSearch[1]) : new Date();
-    var networkSearch = body.match(/network: (.*?)\n/);
-    if (networkSearch && networkSearch[1].includes('github_rss')) {
-      var regex = /href="\/webrender/gi;
-      body = body.replace(regex, 'href="https://github.com/webrender')
-    }
-    github.repos.createFile({
-      owner: process.env.GH_USER,
-      repo: process.env.GH_REPO,
-      path: `_posts/${moment(date).format('YYYY-MM-DD-HH-mm-ss-')}${title}.html`,
-      message: 'post via ifttt-ghpages',
-      content: new Buffer(body).toString('base64')
-    }, function(err, resp) {
-      if (err) {
-        console.log('error: ' + err);
-        res.sendStatus(500);
-      } else {
-        console.log('success: ' + resp);
-        res.sendStatus(200);
-      }
-    });
   }
+
+  // Set incoming post category, or none
+  //
+  var postPath;
+  if (req.query.category === null || req.query.category.length === 0) {
+    postPath = "_posts"
+  } else {
+    postPath = req.query.category + "/_posts"
+  }
+
+  // Cleanup formatting of incoming post, replace special chracters, etc.
+  //
+  //   1. Replace ||| with newlines
+  //   2. Replace |@ with literal |
+  //   3. Replace @@@ with <!--more-->
+  //   4. Strip empty elements at the beginning of any lists
+  //   5. Strip empty elements at the end of any lists
+  //   6. Replace any double <p><p> with a single <p>
+  //   7. Replace any double </p></p> with a single </p>
+  //   8. Replace the sequence .### with just . (since this implies that we don't need ###)
+  //   9. Finally, replace ### with ...
+  //
+  var postContent = req.body.replace(/\|\|\|/g, "\n");
+  postContent = postContent.replace(/\|@/g, "|");
+  postContent = postContent.replace(/@@@/g, "<!--more-->");
+  postContent = postContent.replace(/\[ ("")? ?,/, "[ ");
+  postContent = postContent.replace(/, ?("")? \]/, " ]");
+  postContent = postContent.replace(/<p><p>/, "<p>");
+  postContent = postContent.replace(/<\/p><\/p>/, "</p>");
+  postContent = postContent.replace(/\. *###/, ".");
+  postContent = postContent.replace(/###/, "...");
+
+  // Post date or now.
+  //
+  var postDate;
+  var dateSearch = postContent.match(/date: ?(.+)\n/);
+  if (dateSearch !== null && dateSearch.length > 0) {
+    postDate = chrono.parseDate(dateSearch[1]);
+  } else {
+    postDate = new Date();
+  }
+  console.info("Post date is " + moment(postDate).format("YYYY-MM-DD HH:mm:ss ZZ"));
+  postContent = postContent.replace(/date:.*\n/, "");
+  postContent = postContent.replace(/---\n/, "---\ndate: " + moment(postDate).format("YYYY-MM-DD HH:mm:ss ZZ") + "\n");
+
+  // Figure out the post title. Abort if none is set.
+  //
+  var postTitle;
+  var titleSearch = postContent.match(/title: ?(.+)\n/);
+  if (titleSearch !== null && titleSearch.length > 0) {
+    postTitle = titleSearch[1].trim().replace(/^"/, "").replace(/"$/, "");
+  } else {
+    console.error("Missing or empty 'title' attribute in YAML frontmatter");
+    res.sendStatus(400);
+  }
+  console.info("Post title is \"" + postTitle + "\"");
+  postContent = postContent.replace(/title:.*\n/, "");
+  postContent = postContent.replace(/---\n/, "---\ntitle: |\n  " + postTitle + "\n");
+
+  // Create slug (for use in file names).
+  //
+  var slugTitle = moment(postDate).format("YYYY-MM-DD-") + (accents.remove(postTitle)).toLowerCase().replace(/[^a-z0-9]/g, "-").replace(/-+/g, "-");
+  console.info("Post slug is " + slugTitle);
+
+  // Post content to GitHub
+  //
+  github.repos.createFile({
+    owner: process.env.GH_USER,
+    repo: process.env.GH_REPO,
+    branch: process.env.GH_BRANCH,
+    path: postPath + "/" + slugTitle + ".html",
+    message: "Post automatically pushed from IFTTT via Glitch",
+    content: new Buffer(postContent).toString("base64")
+  }, function(err, resp) {
+    if (err) {
+      console.log("Post creation failed! GitHub responded: " + err);
+      res.sendStatus(500);
+    } else {
+      console.log("Post creation succeeded. GitHub responded: " + resp);
+      res.sendStatus(200);
+    }
+  });
 });
 
-
-// listen for requests :)
+// Listen for incoming requests
+//
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
